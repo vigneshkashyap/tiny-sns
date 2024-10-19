6996d33f6f63ada89e1c0b3bd2ee74e49b116526
Vignesh Kashyap <vignesh.t.kashyap@gmail.com>
2024-10-18 21:21:28 -0500

    Startup script updated

--- tsn-service_start.sh
+++ tsn-service_start.sh
@@ -1,5 +1,5 @@
-GLOG_logtostderr=1 ./coordinator -p 9090
+./coordinator -p 9090 &
 sleep 2s
-GLOG_logtostderr=1 ./tsd -c 1 -s 1 -h localhost -k 9090 -p 10000
+./tsd -c 1 -s 1 -h localhost -k 9090 -p 10000 &
 sleep 1s
-GLOG_logtostderr=1 ./tsd -c 2 -s 1 -h localhost -k 9090 -p 10001
+./tsd -c 2 -s 1 -h localhost -k 9090 -p 10001 &
